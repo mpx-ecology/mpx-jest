aaaccb83d32ceefdb9fdca988ede8db61d831a20
xuegan <xuegan@didiglobal.com>
2021-10-19 20:01:14 +0800

    修改template-compiler部分

--- packages/mpx2-jest/webpack-plugin/loader.js
+++ packages/mpx2-jest/webpack-plugin/loader.js
@@ -1,19 +1,13 @@
 const JSON5 = require('json5')
 const path = require('path')
 const parseComponent = require('./parser')
-const createHelpers = require('./helpers')
 const loaderUtils = require('loader-utils')
 const parseRequest = require('./utils/parse-request')
-const matchCondition = require('./utils/match-condition')
 const fixUsingComponent = require('./utils/fix-using-component')
-const addQuery = require('./utils/add-query')
 const normalize = require('./utils/normalize')
 const templateCompiler = require('./template-compiler/index')
 const babel = require("@babel/core")
-
-const mkdirp = require('mkdirp')
 const fs = require('fs')
-const getDirName = require('path').dirname
 const transformedFiles = new Map
 // content, resource, cb
 //src, filePath, jestConfig
@@ -37,7 +31,6 @@ module.exports = function (src, filePath, jestConfig) {
   const packageName = queryObj.packageName || mpx.currentPackageRoot || 'main'
   const pagesMap = mpx.pagesMap
   const componentsMap = mpx.componentsMap[packageName]
-  const resolveMode = mpx.resolveMode
   const projectRoot = mpx.projectRoot
   const mode = mpx.mode
   const env = mpx.env
@@ -46,8 +39,6 @@ module.exports = function (src, filePath, jestConfig) {
   const globalSrcMode = mpx.srcMode
   const localSrcMode = queryObj.mode
   const srcMode = localSrcMode || globalSrcMode
-  const vueContentCache = mpx.vueContentCache || new Set()
-  const autoScope = matchCondition(resourcePath, mpx.autoScopeRules)
   // 整体页面构建后输出产物
   let outputRes = {
     script: '',
@@ -74,14 +65,6 @@ module.exports = function (src, filePath, jestConfig) {
     }
   }
 
-  // let ctorType = 'app'
-  // if (pagesMap[resourcePath]) {
-  //   // page
-  //   ctorType = 'page'
-  // } else if (componentsMap[resourcePath]) {
-  //   // component
-  //   ctorType = 'component'
-  // }
   ctorType = 'component'
   // mock loaderContext
   const loaderContext = {
@@ -90,46 +73,10 @@ module.exports = function (src, filePath, jestConfig) {
     resource
   }
 
-  function writeFile(path, contents, cb) {
-    return new Promise((resolve) => {
-      mkdirp(getDirName(path), function (err) {
-        if (err) return cb(err);
-
-        fs.writeFile(path, contents, () => {
-          cb && cb()
-          resolve()
-        });
-      });
-    })
-  }
-
-  const stringifyRequest = r => loaderUtils.stringifyRequest(loaderContext, r)
   const isProduction = this.minimize || process.env.NODE_ENV === 'production'
-  const options = loaderUtils.getOptions(this) || {}
-  const processSrcQuery = (src, type) => {
-    const localQuery = Object.assign({}, queryObj)
-    // style src会被特殊处理为全局复用样式，不添加resourcePath，添加isStatic及issuerResource
-    if (type === 'styles') {
-      localQuery.isStatic = true
-      localQuery.issuerResource = this.resource
-    } else {
-      localQuery.resourcePath = resourcePath
-    }
-    if (type === 'json') {
-      localQuery.__component = true
-    }
-    return addQuery(src, localQuery)
-  }
-
 
   //TODO 待调整
   const moduleId = 'm' + filePath
-
-  const needCssSourceMap = (
-    !isProduction &&
-    this.sourceMap &&
-    options.cssSourceMap !== false
-  )
 
   const parts = parseComponent(content, {
     filePath,
@@ -139,26 +86,15 @@ module.exports = function (src, filePath, jestConfig) {
     env
   })
 
-  let output = ''
   const callback = () => {
-    // return
-    // cb(outputRes)
   }
-
-  // web输出模式下没有任何inject，可以通过cache直接返回，由于读取src json可能会新增模块依赖，需要在之后返回缓存内容
-  if (vueContentCache.has(filePath)) {
-    return callback(null, vueContentCache.get(filePath))
-  }
-  // 只有ali才可能需要scoped
   const hasScoped = false
   const templateAttrs = parts.template && parts.template.attrs
   const hasComment = templateAttrs && templateAttrs.comments
   const isNative = false
 
   let usingComponents = [].concat(Object.keys(mpx.usingComponents))
-
   let componentGenerics = {}
-
   if (parts.json && parts.json.content) {
     try {
       let ret = JSON5.parse(parts.json.content)
@@ -174,26 +110,6 @@ module.exports = function (src, filePath, jestConfig) {
     }
   }
 
-  const {
-    getRequire,
-    getRequireForSrc
-  } = createHelpers({
-    loaderContext,
-    options,
-    moduleId,
-    hasScoped,
-    hasComment,
-    usingComponents,
-    needCssSourceMap,
-    srcMode,
-    isNative,
-    projectRoot
-  })
-
-  // 触发webpack global var 注入
-  output += 'global.currentModuleId\n'
-
-  // todo loader中inject dep比较危险，watch模式下不一定靠谱，可考虑将import改为require然后通过修改loader内容注入
   // 注入模块id及资源路径
   let globalInjectCode = `global.currentModuleId = ${JSON.stringify(moduleId)}\n`
   if (!isProduction) {
@@ -202,7 +118,6 @@ module.exports = function (src, filePath, jestConfig) {
   // TODO i18n 延后处理
   if (ctorType === 'app' && i18n && !mpx.forceDisableInject) {
     globalInjectCode += `global.i18n = ${JSON.stringify({locale: i18n.locale, version: 0})}\n`
-
     const i18nMethodsVar = 'i18nMethods'
     const i18nWxsPath = normalize.lib('runtime/i18n.wxs')
     const i18nWxsLoaderPath = normalize.lib('wxs/wxs-i18n-loader.js')
@@ -239,9 +154,8 @@ module.exports = function (src, filePath, jestConfig) {
   }))}\n`
 
   // <script>
-  output += '/* script */\n'
+  outputRes.script += '/* script */\n'
   let scriptSrcMode = srcMode
-  let scriptLang = 'js'
   const script = parts.script
   if (script) {
     scriptSrcMode = script.mode || scriptSrcMode
@@ -291,66 +205,20 @@ module.exports = function (src, filePath, jestConfig) {
   }
 
   // styles
-  output += '/* styles */\n'
-  let cssModules
+  outputRes.style += '/* styles */\n'
   if (parts.styles.length) {
     let styleInjectionCode = ''
-    parts.styles.forEach((style, i) => {
-      let scoped = hasScoped ? (style.scoped || autoScope) : false
-      let requireString
-      // require style
-      if (style.src) {
-        style.src = processSrcQuery(style.src, 'styles')
-        requireString = getRequireForSrc('styles', style, -1, scoped)
-      } else {
-        requireString = getRequire('styles', style, i, scoped)
-      }
-      const hasStyleLoader = requireString.indexOf('style-loader') > -1
-      const invokeStyle = code => `${code}\n`
-
-      const moduleName = style.module === true ? '$style' : style.module
-      // setCssModule
-      if (moduleName) {
-        if (!cssModules) {
-          cssModules = {}
-        }
-        if (moduleName in cssModules) {
-          loaderContext.emitError(
-            'CSS module name "' + moduleName + '" is not unique!'
-          )
-          styleInjectionCode += invokeStyle(requireString)
-        } else {
-          cssModules[moduleName] = true
-
-          if (!hasStyleLoader) {
-            requireString += '.locals'
-          }
-
-          styleInjectionCode += invokeStyle(
-            'this["' + moduleName + '"] = ' + requireString
-          )
-        }
-      } else {
-        styleInjectionCode += invokeStyle(requireString)
-      }
-    })
-    output += styleInjectionCode + '\n'
+    // TODO 添加对css部分的处理，特别是外联css文件
+    outputRes.style += styleInjectionCode + '\n'
   }
 
   // json
-  output += '/* json */\n'
   // 给予json默认值, 确保生成json request以自动补全json
   const json = parts.json || {}
   outputRes.json = json
-  // if (json.src) {
-  //   json.src = processSrcQuery(json.src, 'json')
-  //   output += getRequireForSrc('json', json) + '\n\n'
-  // } else {
-  //   output += getRequire('json', json) + '\n\n'
-  // }
 
   // template
-  output += '/* template */\n'
+  outputRes.template += '/* template */\n'
   const template = parts.template
 
   if (template) {
@@ -364,34 +232,12 @@ module.exports = function (src, filePath, jestConfig) {
       root: projectRoot
     }
     outputRes = templateCompiler.call(this, parts.template.content, outputRes, options)
-    // if (template.src) {
-    //   template.src = processSrcQuery(template.src, 'template')
-    //   output += getRequireForSrc('template', template) + '\n\n'
-    // } else {
-    //   output += getRequire('template', template) + '\n\n'
-    // }
   }
 
   if (!mpx.forceDisableInject) {
-    // TODO global inject 部分要通过 code 拼接方式来实现
-
     outputRes.script = globalInjectCode + '\n' + outputRes.script
-    // TODO babel处理后续改为读取外部babel.config.json
-    // const res = require("@babel/core").transformSync(outputRes.script, {
-    //   plugins: ["@babel/plugin-transform-modules-commonjs"],
-    // });
-    // outputRes.script = res.code
-
-    // const dep = new InjectDependency({
-    //   content: globalInjectCode,
-    //   index: -3
-    // })
-    // TODO 待确认
-    // this._module.addDependency(dep)outputRes.template
   }
 
-  // fs.writeFileSync('src/components/list.jest.js', outputRes.script)
-  const outStr = JSON.stringify(outputRes)
   if (transformedFiles.get(filePath)) {
     const res = require("@babel/core").transformSync(outputRes.script, {
       plugins: ["@babel/plugin-transform-modules-commonjs"],
@@ -400,29 +246,8 @@ module.exports = function (src, filePath, jestConfig) {
       code: res.code
     }
   }
-  // const res = require("@babel/core").transformSync(componentContent.script, {
-  //   plugins: [
-  //     "@babel/plugin-transform-modules-commonjs",
-  //     "@babel/plugin-transform-typescript"
-  //   ],
-  // });
   transformedFiles.set(filePath, true)
   return {
     code: 'module.exports = '+JSON.stringify(outputRes)
   }
-
-  // async.waterfall([
-  //   (callback) => {
-  //     const json = parts.json || {}
-  //     if (json.src) {
-  //       readJsonForSrc(json.src, loaderContext, (err, result) => {
-  //         if (err) return callback(err)
-  //         json.content = result
-  //         callback()
-  //       })
-  //     } else {
-  //       callback()
-  //     }
-  //   },
-  // ], callback)
 }
--- packages/mpx2-jest/webpack-plugin/template-compiler/index.js
+++ packages/mpx2-jest/webpack-plugin/template-compiler/index.js
@@ -1,13 +1,10 @@
 const compiler = require('./compiler')
-const loaderUtils = require('loader-utils')
 const bindThis = require('./bind-this').transform
 const parseRequest = require('../utils/parse-request')
 const matchCondition = require('../utils/match-condition')
 const path = require('path')
 
 module.exports = function (raw, outputRes, options) {
-  // this.cacheable()
-  // const options = loaderUtils.getOptions(this) || {}
   const { resourcePath, queryObj } = parseRequest(this.resource)
   const mainCompilation = {
     __mpx__: {
@@ -113,7 +110,6 @@ ${e.stack}`)
   // todo 此处在loader中往其他模块addDep更加危险，考虑修改为通过抽取后的空模块的module.exports来传递信息
   let globalInjectCode = renderResult.code + '\n'
 
-
   if (meta.computed) {
     globalInjectCode += bindThis(`global.currentInject.injectComputed = {
   ${meta.computed.join(',')}
@@ -126,105 +122,7 @@ ${e.stack}`)
   };\n`
   }
 
-  // const issuer = this._module.issuer
-  // const parser = issuer.parser
-
-  // 同步issuer的dependencies，确保watch中issuer rebuild时template也进行rebuild，使该loader中往issuer中注入的依赖持续有效
-  // issuer.buildInfo.fileDependencies.forEach((dep) => {
-  //   this.addDependency(dep)
-  // })
-  // issuer.buildInfo.contextDependencies.forEach((dep) => {
-  //   this.addContextDependency(dep)
-  // })
-
-  // 删除issuer中上次注入的dependencies，避免issuer本身不需要更新时上次的注入代码残留
-  // issuer.dependencies = issuer.dependencies.filter((dep) => {
-  //   return !dep.templateInject
-  // })
-  // TODO globalInjectCode 拼接方式改为注入形式
-
   outputRes.script = globalInjectCode + '\n' + outputRes.script
-  // const dep = new InjectDependency({
-  //   content: globalInjectCode,
-  //   index: -2
-  // })
 
-  // dep.templateInject = true
-  // issuer.addDependency(dep)
-
-  let isSync = true
-
-  const iterationOfArrayCallback = (arr, fn) => {
-    for (let index = 0; index < arr.length; index++) {
-      fn(arr[index])
-    }
-  }
-
-  const dependencies = new Map()
-
-  const addDependency = dep => {
-    const resourceIdent = dep.getResourceIdentifier()
-    if (resourceIdent) {
-      const factory = compilation.dependencyFactories.get(dep.constructor)
-      if (factory === undefined) {
-        throw new Error(`No module factory available for dependency type: ${dep.constructor.name}`)
-      }
-      let innerMap = dependencies.get(factory)
-      if (innerMap === undefined) {
-        dependencies.set(factory, (innerMap = new Map()))
-      }
-      let list = innerMap.get(resourceIdent)
-      if (list === undefined) innerMap.set(resourceIdent, (list = []))
-      list.push(dep)
-    }
-  }
-
-  for (let module in meta.wxsModuleMap) {
-    isSync = false
-    const src = loaderUtils.urlToRequest(meta.wxsModuleMap[module], options.root)
-    // 编译render函数只在mpx文件中运行，此处issuer的context一定等同于当前loader的context
-    const expression = `require(${loaderUtils.stringifyRequest(this, src)})`
-    const deps = []
-    // parser.parse(expression, {
-    //   current: {
-    //     addDependency: dep => {
-    //       dep.userRequest = module
-    //       deps.push(dep)
-    //     }
-    //   },
-    //   module: issuer
-    // })
-    // issuer.addVariable(module, expression, deps)
-    // iterationOfArrayCallback(deps, addDependency)
-  }
-
-  if (isSync) {
-    return outputRes
-  } else {
-    const callback = this.async()
-
-    // const sortedDependencies = []
-    // for (const pair1 of dependencies) {
-    //   for (const pair2 of pair1[1]) {
-    //     sortedDependencies.push({
-    //       factory: pair1[0],
-    //       dependencies: pair2[1]
-    //     })
-    //   }
-    // }
-
-    // compilation.addModuleDependencies(
-    //   issuer,
-    //   sortedDependencies,
-    //   compilation.bail,
-    //   null,
-    //   true,
-    //   () => {
-    //     callback(null, result)
-    //   }
-    // )
-
-  }
-  console.log(outputRes)
   return outputRes
 }
