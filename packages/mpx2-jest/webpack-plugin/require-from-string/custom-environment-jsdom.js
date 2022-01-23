ec35acbd540350de12407915b562af3aed8c38d9
xuegan <xuegan@didiglobal.com>
2022-01-23 20:12:19 +0800

    更新global全局对象设置

--- packages/mpx2-jest/webpack-plugin/require-from-string/custom-environment-jsdom.js
+++ packages/mpx2-jest/webpack-plugin/require-from-string/custom-environment-jsdom.js
@@ -1,19 +1,28 @@
 const JsdomEnvironment = require('jest-environment-jsdom')
 
+function mergeGlobal(optionsGlobal, global) {
+  try {
+    const optionsGlobalKeys = Object.keys(optionsGlobal)
+    optionsGlobalKeys.forEach((key) => {
+      try {
+        if (!global[key] && optionsGlobal[key]) {
+          global[key] = optionsGlobal[key]
+        }
+      } catch (e) {
+      }
+    })
+  } catch (e) {
+    console.log('jest-environment-jsdom mergeGlobal', e)
+  }
+  return global
+}
+
 class CustomEnvironment extends JsdomEnvironment {
   constructor(config, options) {
     super(config)
     this.global.getCurrentPages = options.global.getCurrentPages
     this.global.getApp = options.global.getApp
-    try {
-      options.global.defs ? null : options.global.defs = {}
-      const keys = Object.keys(options.global.defs)
-      for (let key of keys) {
-        this.global[key] = options.global.defs[key]
-      }
-    } catch (e) {
-      console.error(e)
-    }
+    this.global = mergeGlobal(options.global, this.global)
   }
 }
 
